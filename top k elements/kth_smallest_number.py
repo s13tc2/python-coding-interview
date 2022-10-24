ab6749bfc3ab9c759a2626f0e72d17fc3072cd86
s13tc2 <s13tc2@protonmail.com>
2022-10-23 21:24:06 -0400

    add top k elements question

---
+++ top k elements/kth_smallest_number.py
@@ -0,0 +1,33 @@
+# Given an unsorted array of numbers, find Kth smallest number in it.
+from heapq import *
+
+# O(nlog(k)) time | O(k) space
+def find_Kth_smallest_number(nums, k):
+    max_heap = []
+
+    for i in range(0, k):
+        heappush(max_heap, -nums[i])
+
+    for i in range(k, len(nums)):
+        if nums[i] < -max_heap[0]:
+            heappop(max_heap)
+            heappush(max_heap, -nums[i])
+
+    return -max_heap[0]
+
+
+if __name__ == "__main__":
+    print(
+        "Kth smallest number is: "
+        + str(find_Kth_smallest_number([1, 5, 12, 2, 11, 5], 3))
+    )
+
+    print(
+        "Kth smallest number is: "
+        + str(find_Kth_smallest_number([1, 5, 12, 2, 11, 5], 4))
+    )
+
+    print(
+        "Kth smallest number is: "
+        + str(find_Kth_smallest_number([5, 12, 11, -1, 12], 3))
+    )
